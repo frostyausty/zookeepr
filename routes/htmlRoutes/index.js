a7c66c2e5a38e59cc70992148c8f8f514cedff00
frostyausty <priceaustin62@gmail.com>
2020-11-13 16:52:50 -0600

    restructured the api routes and html routes

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const router = require('express').Router();
+
+//Routes to front end files
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+
+router.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+});
+//End routes to front end files
+
+module.exports = router;
